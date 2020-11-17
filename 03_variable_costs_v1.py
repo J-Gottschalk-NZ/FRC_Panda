2f9a94d07de15fc1aceb1bd62c7b8c337d693e89
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-11-17 20:49:48 +1300

    Variables cost with currency formatting

--- 03_variable_costs_v1.py
+++ 03_variable_costs_v1.py
@@ -31,31 +31,36 @@ def not_blank(question, error):
         return response
 
 
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
+
+
 #  *** Main routine starts here ***
 
 # Set up dictionaries and lists
 
-component_list = []
+item_list = []
 quantity_list = []
 price_list = []
 
 variable_dict = {
-    "Component": component_list,
+    "Item": item_list,
     "Quantity": quantity_list,
     "Price": price_list
 }
 
 # Get user data
-item_name = not_blank("Item name: ", "The item name can't be blank.")
+product_name = not_blank("Product name: ", "The product name can't be blank.")
 
 # loop to get component, quantity and price
-comp_name = ""
-while comp_name.lower() != "xxx":
+item_name = ""
+while item_name.lower() != "xxx":
 
     print()
     # get name, quantity and item
-    comp_name = not_blank("Component name: ", "The component name can't be blank.")
-    if comp_name.lower() == "xxx":
+    item_name = not_blank("Item name: ", "The component name can't be blank.")
+    if item_name.lower() == "xxx":
         break
 
     quantity = num_check("Quantity:",
@@ -67,18 +72,26 @@ while comp_name.lower() != "xxx":
 
 
     # add item, quantity and price to lists
-    component_list.append(comp_name)
+    item_list.append(item_name)
     quantity_list.append(quantity)
     price_list.append(price)
 
+
 variable_frame = pandas.DataFrame(variable_dict)
-variable_frame = variable_frame.set_index('Component')
+variable_frame = variable_frame.set_index('Item')
 
 # Calculate cost of each component
 variable_frame['Cost'] = variable_frame['Quantity'] * variable_frame['Price']
 
+# Find sub total
 variable_sub = variable_frame['Cost'].sum()
 
+# Currency Formatting (uses currency function)
+add_dollars = ['Price', 'Cost']
+for item in add_dollars:
+    variable_frame[item] = variable_frame[item].apply(currency)
+
+# *** Printing Area ****
 
 print(variable_frame)
 
