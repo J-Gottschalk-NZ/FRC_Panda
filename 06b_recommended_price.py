301c1bf8374777837cc3d31a1dd25241cd720724
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-11-18 11:32:01 +1300

    fixed rounding function

--- 06b_recommended_price.py
+++ 06b_recommended_price.py
@@ -1,15 +1,40 @@
 import math
 
 
+# integer that is more than zero.  Takes in custom error message
+def num_check(question, error, num_type):
+    valid = False
+
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
 # rounding function
 def round_up(amount, round_to):
     # rounds amount UP to the specified amount (round_to)
+
+    rounded = round_to * round(math.ceil(amount) / round_to)
+    if rounded < amount:
+        amount += 0.1
+
     return int(round_to * round(math.ceil(amount) / round_to))
 
+
 # Main routine starts here
-how_many = 200
-total = 350
-profit_goal = 200
+how_many = num_check("How many items? ", "Can't be 0", int)
+total = num_check("Total Costs? ", "More than 0", float)
+profit_goal = num_check("Profit Goal? ", "More than 0", float)
+round_to = num_check("Round to nearest...? ", "Can't be 0", int)
 
 sales_needed = total + profit_goal
 
@@ -19,7 +44,6 @@ print("Profit Goal: ${:.2f}".format(profit_goal))
 selling_price = sales_needed / how_many
 print("Selling Price (unrounded): ${:.2f}".format(selling_price))
 
-recommended_price = round_up(selling_price, 1)
+recommended_price = round_up(selling_price, round_to)
 print("Recommended Price: ${:.2f}".format(recommended_price))
-  
-  
+
