9878ef77a5b4491c50a6e743b8675e7296bd9dc6
Jennifer Gottschalk <jgottschalk@masseyhigh.school.nz>
2020-11-18 12:32:48 +1300

    Writes dataframe to txt file

--- 07_export_file.py
+++ 07_export_file.py
@@ -16,12 +16,27 @@ fixed_dict = {
 variable_frame = pandas.DataFrame(variable_dict)
 fixed_frame = pandas.DataFrame(fixed_dict)
 
-heading = "Custom Mugs"
+product_name = "Custom Mugs"
 profit_target = "$100.00"
 required_sales = "$200.00"
 recommended_price = "$5.00"
 
 print(variable_frame)
 
-# Write to file...
+variable_txt = pandas.DataFrame.to_string(variable_frame)
 
+# Write to file...
+# create file to hold data
+file_name = "{}.txt".format(product_name)
+text_file = open(file_name, "w+")
+
+# heading
+text_file.write("**** Fund Raising - {} *****\n\n".format(product_name))
+
+text_file.write(variable_txt)
+
+#
+
+
+# close file
+text_file.close()
